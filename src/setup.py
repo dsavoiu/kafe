2c148e10b010a1a18eb282bb804264c0db391db7
Daniel Savoiu <daniel.savoiu@student.kit.edu>
2013-08-12 18:17:07 +0200

    relaxed dependencies to SciPy 0.9.0, NumPy 1.6.1 and matplotlib 1.1.1

--- src/setup.py
+++ src/setup.py
@@ -12,8 +12,8 @@ setup(
     description='A Python Package for Introduction to Data Analysis in Physics Lab Courses',
     long_description='todo: add long description',#open('README.txt').read(),
     setup_requires=[
-        "NumPy >= 1.7.1",
-        "SciPy >= 0.12.0",
-        "matplotlib >= 1.2.0"
+        "NumPy >= 1.6.1",
+        "SciPy >= 0.9.0",
+        "matplotlib >= 1.1.1"
     ]
 )
