2a599d9b434a66bdbec3ebb9ca0ab794945de605
Daniel Savoiu <daniel.savoiu@student.kit.edu>
2013-09-10 20:51:36 +0200

    version bump

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='kafe',
-    version='0.3alpha1',
+    version='0.4.2',
     author='Daniel Savoiu',
     author_email='danielsavoiu@gmail.com',
     packages=['kafe'],
