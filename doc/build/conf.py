0833dbe9176f6804df50954711052c81d55e3e49
Daniel Savoiu <daniel.savoiu@student.kit.edu>
2016-02-26 19:20:11 +0100

    Fix theme handling on ReadTheDocs.

--- doc/build/conf.py
+++ doc/build/conf.py
@@ -125,7 +125,15 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #html_theme = 'nature'
-html_theme = 'rtd_theme'
+
+# check if the docs are building on ReadTheDocs
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if on_rtd:
+    # use the default theme on RTD
+    html_theme = 'default'
+else:
+    # for private builds, use the "offine" RTD theme
+    html_theme = 'rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
