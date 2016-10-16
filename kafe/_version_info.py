02082715498dde5da046bb7aa1f7d62d27d8ac42
Daniel Savoiu <daniel.savoiu@cern.ch>
2016-10-16 22:19:25 +0200

    Bump version number to 1.2.0

--- kafe/_version_info.py
+++ kafe/_version_info.py
@@ -1,15 +1,14 @@
 '''
 .. module:: _version_info
    :platform: Unix
-   :synopsis: Version 1.1.0 of kafe, release Jun. 2016
+   :synopsis: Version 1.2.0 of kafe, release Jun. 2016
 
 .. moduleauthor:: Daniel Savoiu <danielsavoiu@gmail.com>
                   Guenter Quast <g.quast@kit.edu>
-
 '''
 
 major = 1
-minor = 1
+minor = 2
 revision = 0
 
 def _get_version_tuple():
