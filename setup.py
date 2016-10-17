0babe4f868232c75161770e9b413f0469a2fcb6e
Daniel Savoiu <daniel.savoiu@cern.ch>
2016-10-17 02:18:23 +0200

    some changes to setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,20 @@
 import kafe  # from this directory
 import sys
+import os
 
 from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
-# class for running unit tests
-# from: https://pytest.org/latest/goodpractices.html
+
+# Get the long description from the README file
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                       'README.rst')) as f:
+    long_description = f.read()
 
 setup(
     name='kafe',
     version=kafe.__version__,
-    author='Daniel Savoiu, Guenter Quast',
+    author='Daniel Savoiu, Guenter Quast, Joerg Schindler',
     author_email='danielsavoiu@gmail.com',
     packages=['kafe'],
     package_data={'kafe': ['config/*.conf']},
@@ -19,11 +23,25 @@ setup(
     license='GNU Public Licence',
     description='A Python Package for Introduction to \
         Data Analysis in Physics Lab Courses',
-    long_description='todo: add long description',  # open('README.txt').read()
+    long_description=long_description,
     setup_requires=[
         "NumPy >= 1.7.1",
         "SciPy >= 0.12.0",
         "matplotlib >= 1.5.0",
     ],
-    test_suite = 'unittest2.collector'
+    test_suite = 'unittest.collector',
+    keywords = "data analysis lab courses education students physics fitting minimization",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Topic :: Education',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Visualization',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
